b01f28627cc4b110d9598ee3f37e85612553eaee
Alec <avan-etten@mail.greenriver.edu>
2019-02-03 22:52:00 -0500

    added validation, error messages and fixed errors

--- index.php
+++ index.php
@@ -12,23 +12,30 @@ error_reporting(E_ALL);
 
 //require autoload
 require_once "vendor/autoload.php";
+
 require_once "model/validation.php";
+
 //Create an instance of the Base class
 $f3 = Base::instance();
+
+
 $f3->set('DEBUG', 3);
 //define a default route
 $f3->route('GET|POST /', function() {
+    $_POST = array();
+    session_start();
+    $_SESSION = array();
 
     $view = new View;
     echo $view->render('views/home.html');
 });
 
+//define route for profile form
 $f3->route('GET|POST /form1', function($f3) {
+    session_start();
 
-    if (validateForm1()){
-    $f3->set("SESSION.email", $f3->get("POST.email"));
-    $f3->set("SESSION.state", $f3->get("POST.state"));
-    $f3->set("SESSION.seeking", $f3->get("POST.seeking"));
+    if (!empty($_POST))
+    if (validateForm1($f3)){
 
     $f3->reroute('/form2');
     }
@@ -36,20 +43,31 @@ $f3->route('GET|POST /form1', function($f3) {
     echo $template->render('views/profile.html');
 });
 
+//define route for personal form
 $f3->route('GET|POST /form2', function($f3) {
+    session_start();
+    if (validateForm2()){
+        $f3->reroute('/form3');
+    }
 
     $template = new Template;
     echo $template->render('views/personal.html');
 });
 
+//define route for interests form
 $f3->route('GET|POST /form3', function($f3) {
+    session_start();
+    if(validateForm3()) {
+        $f3->reroute('/profile');
+    }
 
     $template = new Template;
     echo $template->render('views/interests.html');
 });
 
+//define route for profile summary
 $f3->route('GET|POST /profile', function($f3) {
-
+    session_start();
     $template = new Template;
     echo $template->render('views/summary.html');
 });
--- model/validation.php
+++ model/validation.php
@@ -6,17 +6,26 @@
  * Time: 6:17 PM
  */
 
-function validateForm1(){
-    print_r($_POST);
+function validateForm1($f3){
+    //print_r($_POST);
     $bool = true;
-    if (!isset($_POST['email'])){
-        $bool == false;
+    if (!isset($_POST['email']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+        $f3->set("errors['email']", "Please provide a valid email address");
+        $bool = false;
+    } else {
+        $_SESSION['email'] = $_POST['email'];
     }
     if (!isset($_POST['state']) || !validState($_POST['state'])){
-        $bool == false;
+        $f3->set("errors['state']", "What is this? That was not a state");
+        $bool = false;
+    } else {
+        $_SESSION['state'] = $_POST['state'];
     }
-    if (!isset($_POST['seeking[]'])){
-        $bool == false;
+    if (!isset($_POST['seeking'])){
+        $f3->set("errors['seeking']", "You either aren't seeking anything, or have come to the wrong site");
+        $bool = false;
+    } else {
+        $_SESSION['seeking'] = $_POST['seeking'];
     }
 
     return $bool;
@@ -34,9 +43,62 @@ function validState($state){
         'SOUTH CAROLINA', 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT',
         'VIRGIN ISLANDS', 'VIRGINIA', 'WASHINGTON', 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING');
 
-    strtoupper($state);
+    $state = strtoupper($state);
     if (in_array($state, $states)){
         return true;
     }
     return false;
+}
+
+function validateForm2(){
+   // print_r($_POST);
+   // print_r($_SESSION);
+
+    $bool = true;
+    if (!isset($_POST['fName'])){
+        $bool = false;
+    } else {
+        $_SESSION['fName'] = $_POST['fName'];
+    }
+    if (!isset($_POST['lName'])){
+        $bool = false;
+    } else {
+        $_SESSION['lName'] = $_POST['lName'];
+    }
+    if (!isset($_POST['age'])){
+        $bool = false;
+    } else {
+        $_SESSION['age'] = $_POST['age'];
+    }
+    if (!isset($_POST['gender'])){
+        $bool = false;
+    } else {
+        $_SESSION['gender'] = $_POST['gender'];
+    }
+    if (!isset($_POST['phone'])){
+        $bool = false;
+    } else {
+        $_SESSION['phone'] = $_POST['phone'];
+    }
+    return $bool;
+}
+
+function validateForm3(){
+   // print_r($_POST);
+   // print_r($_SESSION);
+
+    $bool = true;
+    if (!isset($_POST['basic'])){
+    $bool = false;
+    } else {
+        $_SESSION['basic'] = $_POST['basic'];
+    }
+    if (!isset($_POST['complicated'])){
+        $bool = false;
+    } else {
+        $_SESSION['complicated'] = $_POST['complicated'];
+    }
+
+    return $bool;
+
 }
