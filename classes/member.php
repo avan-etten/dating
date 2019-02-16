201fe0fed26269dcc43be2322270195daa7ed143
Alec <avan-etten@mail.greenriver.edu>
2019-02-16 18:54:57 -0500

    added member class, for regular members

---
+++ classes/member.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vanteet
+ * Date: 2/16/2019
+ * Time: 3:47 PM
+ */
+
+class Member
+{
+
+    private $_fname;
+    private $_lname;
+    private $_age;
+    private $_gender;
+    private $_phone;
+    private $_email;
+    private $_state;
+    private $_seeking;
+    private $_bio;
+
+    public function __construct($fname, $lname, $age, $gender, $phone){
+        $this->_fname = $fname;
+        $this->_lname = $lname;
+        $this->_age = $age;
+        $this->_gender = $gender;
+        $this->_phone = $phone;
+    }
+
+    /**
+     * @return String first name
+     */
+    public function getFname()
+    {
+        return $this->_fname;
+    }
+
+    /**
+     * @param String $fname
+     */
+    public function setFname($fname)
+    {
+        $this->_fname = $fname;
+    }
+
+    /**
+     * @return String last name
+     */
+    public function getLname()
+    {
+        return $this->_lname;
+    }
+
+    /**
+     * @param String $lname
+     */
+    public function setLname($lname)
+    {
+        $this->_lname = $lname;
+    }
+
+    /**
+     * @return int age
+     */
+    public function getAge()
+    {
+        return $this->_age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge($age)
+    {
+        $this->_age = $age;
+    }
+
+    /**
+     * @return String gender.
+     */
+    public function getGender()
+    {
+        return $this->_gender;
+    }
+
+    /**
+     * @param String $gender
+     */
+    public function setGender($gender)
+    {
+        $this->_gender = $gender;
+    }
+
+    /**
+     * @return String phone number
+     */
+    public function getPhone()
+    {
+        return $this->_phone;
+    }
+
+    /**
+     * @param String $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->_phone = $phone;
+    }
+
+    /**
+     * @return String email
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * @param String $email
+     */
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return String state
+     */
+    public function getState()
+    {
+        return $this->_state;
+    }
+
+    /**
+     * @param String $state
+     */
+    public function setState($state)
+    {
+        $this->_state = $state;
+    }
+
+    /**
+     * @return String seeking
+     */
+    public function getSeeking()
+    {
+        return $this->_seeking;
+    }
+
+    /**
+     * @param String $seeking
+     */
+    public function setSeeking($seeking)
+    {
+        $this->_seeking = $seeking;
+    }
+
+    /**
+     * @return String bio
+     */
+    public function getBio()
+    {
+        return $this->_bio;
+    }
+
+    /**
+     * @param String $bio
+     */
+    public function setBio($bio)
+    {
+        $this->_bio = $bio;
+    }
+
+
+}
