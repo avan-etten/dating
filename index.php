ece68413fe24c6e315da33173ac3ca7e0fc276bb
Alec <avan-etten@mail.greenriver.edu>
2019-02-16 21:40:44 -0500

    added class to session variable

--- index.php
+++ index.php
@@ -16,19 +16,19 @@ require_once "vendor/autoload.php";
 require_once "model/validation.php";
 
 //Connect to DB
-require '/home/avanette/config.php';
+//require '/home/avanette/config.php';
 
 
 //Create an instance of the Base class
 $f3 = Base::instance();
-try {
+/*try {
     //Instantiate a database object
     $dbh= new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
     //echo 'Connected to database!';
 }
 catch(PDOException$e) {
     echo$e->getMessage();
-}
+}*/
 
 $f3->set('DEBUG', 3);
 //define a default route
@@ -44,43 +44,56 @@ $f3->route('GET|POST /', function() {
 //define route for profile form
 $f3->route('GET|POST /form1', function($f3) {
     session_start();
+   // print_r($_POST);
+    if (!empty($_POST)) {
+        if (validateForm1($f3)) {
+            if (!isset($_POST['premium'])) {
+                $member = new Member($_POST['fName'], $_POST['lName'],
+                    $_POST['age'], $_POST['gender'], $_POST['phone']);
+            } else {
+                $member = new PremiumMember($_POST['fName'], $_POST['lName'],
+                    $_POST['age'], $_POST['gender'], $_POST['phone']);
+            }
+            $_SESSION['member'] = $member;
 
-    if (!empty($_POST))
-    if (validateForm1($f3)){
-        if (!isset($_POST['premium'])) {
-            $_SESSION['member'] = new Member($_POST['fname'], $_POST['lname'],
-                $_POST['age'], $_POST['gender'], $_POST['email']);
-        } else {
-            $_SESSION['member'] = new PremiumMember($_POST['fname'], $_POST['lname'],
-                $_POST['age'], $_POST['gender'], $_POST['email']);
+            $f3->reroute('/form2');
         }
-
-    $f3->reroute('/form2');
     }
     $template = new Template;
-    echo $template->render('views/profile.html');
+    echo $template->render('views/personal.html');
 });
 
 //define route for personal form
 $f3->route('GET|POST /form2', function($f3) {
     session_start();
-
+   // print_r($_SESSION);
     if (!empty($_POST))
     if (validateForm2($f3)){
+
+        $member = $_SESSION['member'];
+        $member->setEmail($_POST['email']);
+        $member->setState($_POST['state']);
+        $member->setSeeking($_POST['seeking']);
+        $_SESSION['member'] = $member;
         $f3->reroute('/form3');
     }
 
     $template = new Template;
-    echo $template->render('views/personal.html');
+    echo $template->render('views/profile.html');
 });
 
 //define route for interests form
 $f3->route('GET|POST /form3', function($f3) {
     session_start();
 
-    if (!empty($_POST))
-    if(validateForm3($f3)) {
-        $f3->reroute('/profile');
+    if (!empty($_POST)) {
+        if (validateForm3($f3)) {
+            $premiumMember = $_SESSION['member'];
+            $premiumMember->setBasicInterests($_POST['basic']);
+            $premiumMember->setComplicatedInterests($_POST['complicated']);
+            $_SESSION['member'] = $premiumMember;
+            $f3->reroute('/profile');
+        }
     }
 
     $template = new Template;
@@ -90,7 +103,19 @@ $f3->route('GET|POST /form3', function($f3) {
 //define route for profile summary
 $f3->route('GET|POST /profile', function($f3) {
     session_start();
-    $f3->set('interests', $_SESSION['basic']);
+    //print_r($_SESSION);
+    $member = $_SESSION['member'];
+    $_SESSION['fName'] = $member->getFname();
+    $_SESSION['lName'] = $member->getLname();
+    $_SESSION['age'] = $member->getAge();
+    $_SESSION['gender'] = $member->getGender();
+    $_SESSION['email'] = $member->getEmail();
+    $_SESSION['phone'] = $member->getPhone();
+    $_SESSION['state'] = $member->getState();
+    $_SESSION['seeking'] = $member->getSeeking();
+    $_SESSION['basic'] = $member->getBasicInterests();
+    $_SESSION['complicated'] = $member->getComplicatedInterests();
+
     $template = new Template;
     echo $template->render('views/summary.html');
 });
